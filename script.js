3d34507d6b729cc5ce73bee1dfde26921de5c1f7
FrancarriYT <francarri2pro@gmail.com>
2023-05-18 19:15:49 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function ShowAlert(){
+    alert("hola lolero promedio");
+}
+
+document.querySelector("button.button-menu-toggle")
+    .addEventListener("click", function() {
+           document.querySelector(".nav-links").
+                      classList.toggle("nav-links-responsive")})
